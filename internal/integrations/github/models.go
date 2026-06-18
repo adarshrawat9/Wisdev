689473fba33605559305fb871c97031e80a891b6
adarshrawat9 <adarshrawat9368@gmail.com>
2026-06-18 19:12:34 +0530

    add JSON tags to response models

--- internal/integrations/github/models.go
+++ internal/integrations/github/models.go
@@ -3,21 +3,21 @@ package github
 import "time"
 
 type User struct {
-	Name        string
-	Bio         string
-	AvatarURL   string
-	Followers   int
-	Following   int
-	PublicRepos int
+	Name        string `json:"name"`
+	Bio         string `json:"bio"`
+	AvatarURL   string `json:"avatar_url"`
+	Followers   int    `json:"followers"`
+	Following   int    `json:"following"`
+	PublicRepos int    `json:"public_repos"`
 }
 
 type Repository struct {
-	Name        string
-	Description string
-	Stars       int
-	Forks       int
-	Language    string
-	URL         string
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
+	Name        string      `json:"name"`
+	Description string      `json:"description"`
+	Stars       int         `json:"stars"`
+	Forks       int         `json:"forks"`
+	Language    string      `json:"language"`
+	URL         string      `json:"url"`
+	CreatedAt   time.Time   `json:"created_at"`
+	UpdatedAt   time.Time   `json:"updated_at"`
 }
